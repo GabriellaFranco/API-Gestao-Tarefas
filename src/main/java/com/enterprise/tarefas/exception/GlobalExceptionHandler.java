64b615ee90c111d647a26f7bf434aed9e6391725
Gabriella Franco <gabriella-nena@hotmail.com>
2025-06-12 22:09:52 -0300

    GlobalExceptionHandler criado

---
+++ src/main/java/com/enterprise/tarefas/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.enterprise.tarefas.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException exception) {
+        ErrorResponse error = new ErrorResponse();
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exception.getMessage());
+        error.setTimestamp(LocalDateTime.now().toString());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+}
