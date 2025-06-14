233855f925accd799bce6c3b88c490640cfc2874
Gabriella Franco <gabriella-nena@hotmail.com>
2025-06-14 20:30:05 -0300

    Banco H2 para testes configurado

--- src/test/java/com/enterprise/tarefas/controller/TarefaControllerTests.java
+++ src/test/java/com/enterprise/tarefas/controller/TarefaControllerTests.java
@@ -28,6 +28,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@ActiveProfiles("test")
 @WebMvcTest(TarefaController.class)
 public class TarefaControllerTests {
 
